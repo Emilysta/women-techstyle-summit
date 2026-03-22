76d28096d61b8c7e2e962a4e7d6d780f68bf1ee2
Emilia Starczyk (MB) <emiliastarczyk@gmail.com>
2026-03-22 20:44:01 +0100

    feat: Added test for failing accessibility

--- tests/axe.spec.ts
+++ tests/axe.spec.ts
@@ -16,3 +16,22 @@ test.describe('homepage', () => {
     expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
+
+
+test.describe('accessibility-testing-broken-slide', () => {
+  test('should have automatically detectable accessibility issues', async ({ page }, testInfo) => {
+    await page.goto('/accessibility-testing-broken-slide');
+
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .analyze();
+
+    await testInfo.attach('accessibility-scan-results', {
+      body: JSON.stringify(accessibilityScanResults, null, 2),
+      contentType: 'application/json'
+    });
+
+    expect(accessibilityScanResults.violations).toHaveLength(2);
+    expect(accessibilityScanResults.violations.at(0)?.id).toEqual('button-name');
+    expect(accessibilityScanResults.violations.at(1)?.id).toEqual('color-contrast');
+  });
+});
